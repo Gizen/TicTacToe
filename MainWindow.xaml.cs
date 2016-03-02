12705acaac439060cec3950ce5f6bff2b4269db1
Gizen <billingsmark1999@gmail.com>
2016-03-02 09:04:37 -0700

    Basic Method for X and O has been added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,89 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        int playerTurn = 0;
+
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            gameButton.IsEnabled = false;
+            gameButton1.IsEnabled = false;
+            gameButton2.IsEnabled = false;
+            gameButton3.IsEnabled = false;
+            gameButton4.IsEnabled = false;
+            gameButton5.IsEnabled = false;
+            gameButton6.IsEnabled = false;
+            gameButton7.IsEnabled = false;
+            gameButton8.IsEnabled = false;
+
+            string playerX = textBoxPlayerX.Text;
+            string playerO = textBoxPlayerO.Text;
+
+
+        }
+
+        private void buttonStartGame_Click(object sender, RoutedEventArgs e)
+        {
+            gameButton.IsEnabled = true;
+            gameButton1.IsEnabled = true;
+            gameButton2.IsEnabled = true;
+            gameButton3.IsEnabled = true;
+            gameButton4.IsEnabled = true;
+            gameButton5.IsEnabled = true;
+            gameButton6.IsEnabled = true;
+            gameButton7.IsEnabled = true;
+            gameButton8.IsEnabled = true;
+
+        }
+
+        private void XO()
+        {
+            if (playerTurn == 0)
+            {
+                gameButton.Content = "X";
+                playerTurn += 1;
+            }
+            else
+            {
+                gameButton.Content = "O";
+                playerTurn -= 1;
+            }
+
+
+
+        }
+
+        private void gameButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void gameButton1_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
+//Layout Window
+//DONE
+
+//Buttons cant be clicked before game starts
+//DONE
+
+//TextBlocks display x or o
+
+//On click display whos turn
+
+//calc winner
+
+//Detecty no winner
+
+//track whos turn
+
+//aftereach click check for win
+
+// do not disable buttons
+
+//on start dont change names
